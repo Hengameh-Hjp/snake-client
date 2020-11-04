eec15833b425b02fce884e4046f6542076a58263
Hana <67531901+hajhana@users.noreply.github.com>
2020-11-03 21:58:09 -0500

    Add files via upload

--- play.js
+++ play.js
@@ -5,14 +5,19 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.0.209',
+    host: '10.0.2.15',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+
+    conn.on('data', (data) => {
+      console.log('Server says: ', data);
+    });
+
   return conn;
 }
 
 console.log('Connecting ...');
-connect();
+conn = connect();
